144eb93ee1aee200e7ffead9b698017af6259a35
nethanhan <nethanhan@nethanhandeMacBook-Pro-2.local>
2017-06-04 17:16:06 +0800

    project create

---
+++ YKKline/YKKline/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  YKKline
+//
+//  Created by nethanhan on 2017/6/4.
+//  Copyright © 2017年 nethanhan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
